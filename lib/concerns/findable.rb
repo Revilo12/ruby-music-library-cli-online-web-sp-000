e6a52402c6eae8543fc9b8e157cc7d1e57fc0c30
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-20 14:06:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,20 +1,23 @@
-# module Concerns::Findable
-#
-#   def find_by_name(name)
-#     all.find do |obj|
-#       obj.songs.any? do |song|
-#         song.name == name
-#       end
-#     end
-#   end
-#
-#   def find_or_create_by_name(name)
-#     found = self.find_by_name(name)
-#     if found
-#       found
-#     else
-#       Songs.create(name)
-#     end
-#   end
-#
-# end
+require './config/environment.rb'
+module Concerns::Findable
+
+  def create(name)
+    obj = self.new(name)
+    obj.save
+    obj
+  end
+
+  def find_by_name(name)
+    self.all.find {|obj| obj.name.downcase == name.downcase}
+  end
+
+  def find_or_create_by_name(name)
+    found = find_by_name(name)
+    if !(found)
+      create(name)
+    else
+      found
+    end
+  end
+
+end
